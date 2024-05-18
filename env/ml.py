01f3a71c441eb8c2095f0723349341f95968062e
unknown <ckirk0527@windstream.net>
2024-05-18 10:48:37 -0700

    neww

--- env/ml.py
+++ env/ml.py
@@ -8,6 +8,6 @@ import sklearn
 from scipy import constants
 print(constants.liter)
 
-df = pd.read_csv('')
+df = pd.read_csv("C:\\Users\\Diane\\Downloads\\customers-100.csv")
 print(df.to_string())
   
